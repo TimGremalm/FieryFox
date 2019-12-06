b54e14a99a83a47cdc1e0a839568cd8fe8376045
Tim Gremalm <tim@gremalm.se>
2019-12-06 15:08:24 +0100

    Update fire effect

--- FieryFox.ino
+++ FieryFox.ino
@@ -4,7 +4,7 @@
 //Led strip WS2812
 #define WS2812PIN 3
 #define WS2812PIXELS 20
-#define POTMIN 10
+#define POTMIN 20
 #define POTMAX 1010
 
 int potLightLevelLast = -10;
@@ -15,34 +15,30 @@ void setup() {
 	Serial.begin(115200);
 	Serial.println("Start Elements light");
 	ws2812fx.init();
-	ws2812fx.setBrightness(1);
+	ws2812fx.setBrightness(100);
+	ws2812fx.setSpeed(1200);
+	ws2812fx.setColor(0xFF6600);
+	ws2812fx.setMode(FX_MODE_FIRE_FLICKER_INTENSE);
 	ws2812fx.start();
 }
 
 void checkLightLevelPot() {
 	int potLightLevel = analogRead(A6);
+	// Cap input
 	if (potLightLevel < POTMIN) {
 		potLightLevel = POTMIN;
 	}
 	if (potLightLevel > POTMAX) {
 		potLightLevel = POTMAX;
 	}
-	potLightLevel = map(potLightLevel, 10, 1010, 0, 255);
-	//To avaoid flicker, only change level if +/-4 steps
+	potLightLevel = map(potLightLevel, POTMIN, POTMAX, 0, 255);
+	// To avaoid flicker, only change level if +/-4 steps
 	if ((potLightLevelLast-potLightLevel > 4) || (potLightLevelLast-potLightLevel < -4)) {
 		Serial.print("Light level changed to ");
 		Serial.println(potLightLevel);
 		ws2812fx.setBrightness(potLightLevel);
 		potLightLevelLast = potLightLevel;
 	}
-}
-
-void changeMode() {
-	Serial.println("Changed mode");
-	//ws2812fx.setSpeed(speed*14);
-	ws2812fx.setSpeed(15000);
-	ws2812fx.setColor(0xFF6600);
-	ws2812fx.setMode(FX_MODE_FIRE_FLICKER_INTENSE);
 }
 
 void tick() {
